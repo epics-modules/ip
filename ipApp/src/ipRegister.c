ac9acd6327795edd1190946172c9987d2d0d2fed
MarkRivers <rivers@cars.uchicago.edu>
2003-07-08 19:51:50 +0000

    Registration code

---
+++ ipApp/src/ipRegister.c
@@ -0,0 +1,14 @@
+/* ipRegister.c */
+/* This file forces the Keithley2kDMM.st object file to be included when the ip library is linked with */
+
+#include "epicsExport.h"
+#include "seqCom.h"
+extern struct seqProgram Keithley2kDMM;
+
+void ipRegister(void)
+{
+    struct seqProgram junk;
+    junk = Keithley2kDMM;
+}
+
+epicsExportRegistrar(ipRegister);
