8c30db1a15983e150fc839479c13c3d6a300fcae
Kurt Goetze <kurtgoetze@users.noreply.github.com>
2003-07-09 18:33:15 +0000

    kag- general version with bind call

--- ipApp/src/devSoEurotherm.cc
+++ ipApp/src/devSoEurotherm.cc
@@ -1,15 +1,27 @@
 
 // $Log: not supported by cvs2svn $
+// Revision 1.2  2002/10/24 02:58:30  rivers
+// Added bind call for DevMpf
+//
 // Revision 1.1  2002/01/07 16:06:35  sluiter
 // Copied from synApps R4.3
 //
 // Revision 1.3  1995/04/12  16:23:45  jbk
 // new stuff added
 //
-
+// This module implements the Eurotherm EI-Bisynch protocol to talk
+// to selected Eurotherm temperature cntrollers.  At present, selected
+// commands for 800-series and 2000-series controllers are known to
+// work.  This module makes no attempt at the MODBUS protocol also
+// supported by Eurotherm 2000-series temperature controllers.  You
+// must ensure that the device is using bisynch.
+//
 // Author: Tim Mooney (based on code written by Jim Kowalkowski)
 // Revised: 09/08/97
 // Revised 09/05/01 Tim Mooney, converted to MPF
+// Reviced 02/14/03 Tim Mooney, can specify group and local address in
+//                              vmeio parm string
+// Revised 06/06/03 Kurt Goetze, merged Tim's changes with Mark's (bind call)
 
 #include <stdlib.h>
 #include <stddef.h>
@@ -64,6 +76,8 @@ private:
 	char term[10];
 	int termlen;
 	int timeout;
+	int group_address;
+	int local_address;
 };
 
 MAKE_DSET(devSoEurotherm,SoEurotherm::dev_init)
@@ -71,6 +85,7 @@ MAKE_DSET(devSoEurotherm,SoEurotherm::dev_init)
 long SoEurotherm::dev_init(void* v)
 {
 	stringoutRecord* pr = (stringoutRecord*)v;
+/*	new SoEurotherm((dbCommon*)pr,&(pr->out)); */
 	SoEurotherm *pSoEurotherm = new SoEurotherm((dbCommon*)pr,&(pr->out));
 	pSoEurotherm->bind();
 	return 0;
@@ -110,6 +125,14 @@ SoEurotherm::SoEurotherm(dbCommon* pr,DBLINK* l) : DevMpf(pr,l,false)
 		if ((p = strstr(pio->parm, "TO="))) {
 			timeout = atoi(&p[3]);
 		}
+
+		if ((p = strstr(pio->parm, "GAD="))) {
+			group_address = atoi(&p[4]);
+		}
+
+		if ((p = strstr(pio->parm, "LAD="))) {
+			local_address = atoi(&p[4]);
+		}
 	}
 	DEBUG(2,"SoEurotherm::SoEurotherm term = '%s'\n", term);
 	DEBUG(2,"SoEurotherm::SoEurotherm buffer_start_index = %d\n", buffer_start_index);
@@ -129,10 +152,10 @@ long SoEurotherm::startIO(dbCommon* pr)
 	int max_size = sizeof(so->val) + sizeof(term);
 	message->allocValue(max_size);
 	buffer[0] = 4; // EOT
-	buffer[1] = '0'; // group address
-	buffer[2] = '0'; // group address repeated
-	buffer[3] = '0'; // local address
-	buffer[4] = '0'; // local address repeated
+	buffer[1] = '0' + group_address; // group address
+	buffer[2] = '0' + group_address; // group address repeated
+	buffer[3] = '0' + local_address; // local address
+	buffer[4] = '0' + local_address; // local address repeated
 	buffer[5] = 0;
 
 	strncat(buffer, &(so->val[buffer_start_index]),
