f4ad03e0be9044431a8b58904a8577dce53e1539
MarkRivers <rivers@cars.uchicago.edu>
2005-12-15 19:52:07 +0000

    Broke st.cmd into ip_serial.cmd and local_serial.cmd

--- iocBoot/iocIpTest/st.cmd
+++ iocBoot/iocIpTest/st.cmd
@@ -1,20 +1,18 @@
+< envPaths
+
 # Tell EPICS all about the record types, device-support modules, drivers,
 # etc. in this build from ipApp
 dbLoadDatabase("../../dbd/ip.dbd")
-registerRecordDeviceDriver(pdbbase)
+ip_registerRecordDeviceDriver(pdbbase)
+# For WIN32 comment out the above lines and use these instead
+#dbLoadDatabase("../../dbd/ipWin32.dbd")
+#ipWin32_registerRecordDeviceDriver(pdbbase)
 
-routerInit
-localMessageRouterStart(0)
+# Load IP serial port stuff
+< ip_serial.cmd
 
-# Set up 2 serial ports
-initTtyPort("serial1", "/dev/ttyS0", 9600, "N", 1, 8, "N", 1000)
-initTtyPort("serial2", "/dev/ttyS1", 19200, "N", 1, 8, "N", 1000)
-initSerialServer("serial1", "serial1", 1000, 20, "")
-initSerialServer("serial2", "serial2", 1000, 20, "")
-
-# Serial 1 Keithley Multimeter
-dbLoadRecords("../../../ip/ipApp/Db/Keithley2kDMM_mf.db", "P=ipTest:,Dmm=DMM1,C=0,SERVER=serial1")
-#dbLoadRecords("../../../ip/ipApp/Db/serial_OI_block.db", "P=ipTest:,C=0,N=1,SERVER=serial1")
+# Load local serial port stuff
+< local_serial.cmd
 
 iocInit
 
